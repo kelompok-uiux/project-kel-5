511482f873287cf10ce379de22b48647d3f7036e
DvaDvaD <davidlou0810@gmail.com>
2023-11-10 13:46:52 +0700

    add test components

---
+++ src/app/components/TestComponent.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const TestComponent = () => {
+  return <div className="text-blue-500">This is a test component</div>
+}
+
+export default TestComponent
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import TestComponent from "./components/TestComponent"
+
 export default function Home() {
-  return <div className="text-red-500">Test</div>
+  return (
+    <div className="text-red-500">
+      <p>Test</p>
+      <TestComponent />
+    </div>
+  )
 }
