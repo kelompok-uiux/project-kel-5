6cfce633fd4ebdcbec734c29872466feac530785
DvaDvaD <davidlou0810@gmail.com>
2023-11-10 13:40:57 +0700

    project setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,3 @@
-"use client"
-import Image from 'next/image'
-import NoteCard from './components/NoteCard'
-import Editor from './components/TesEditor'
-import NoteCardModal from './components/NoteCardModal'
-import React, { ReactNode, useState } from 'react'
-import NoteCardList from './components/NoteCardList'
-
-
-
 export default function Home() {
-
-
- 
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <NoteCardModal/>
-
-        <NoteCardList/>
-
-        <Editor/>
-    
-    </main>
-  )
+  return <div className="z-10 text-red-500"> Test</div>
 }
