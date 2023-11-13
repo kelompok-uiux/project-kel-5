6f7f2389b85b91f5c0af08bd629002bcb1cd52dc
azbagas <azharbagaskara1@gmail.com>
2023-11-13 12:22:02 +0700

    Initial branch bagas

--- src/app/components/TestComponent.tsx
+++ src/app/components/TestComponent.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 const TestComponent = () => {
-  return <div className="text-blue-500">This is a test component</div>
+  return <div className="h-screen text-blue-500">This is a test component</div>
 }
 
 export default TestComponent
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import TestComponent from "./components/TestComponent"
+import Header from "./components/Header"
 
 export default function Home() {
   return (
     <div className="text-red-500">
-      <p>Test</p>
+      <Header />
       <TestComponent />
     </div>
   )
