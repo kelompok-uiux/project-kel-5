a1769d60f0bfb3458ba4770df1e6feadae4a2346
azbagas <azharbagaskara1@gmail.com>
2023-11-13 15:52:17 +0700

    Header dasar

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,63 +1,71 @@
-import React from "react"
-
-// Buat sebuah addEventListener untuk
+"use client"
+import React, { useEffect } from "react"
+import { BiSearch } from "react-icons/bi"
+import { PiListBold } from "react-icons/pi"
+import { IoIosArrowDown } from "react-icons/io"
+import { HiUserCircle } from "react-icons/hi"
 
 const Header = () => {
+  // Buat toogle dropdown hamburger list
+  const handleClick = () => {
+    const element = document.getElementById("navbar-search")
+    if (element && element.classList.contains("hidden")) {
+      element.classList.remove("hidden")
+      element.classList.add("block")
+    } else if (element) {
+      element.classList.remove("block")
+      element.classList.add("hidden")
+    }
+  }
+
+  // Buat toggle dropdown profile
+  const dropdown = () => {
+    const element = document.getElementById("dropdownNavbar")
+    if (element && element.classList.contains("hidden")) {
+      element.classList.remove("hidden")
+      element.classList.add("block")
+    } else if (element) {
+      element.classList.remove("block")
+      element.classList.add("hidden")
+    }
+  }
+
+  // Buat close dropdown profile ketika klik diluar dropdown profile
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      const dropdownNavbar = document.getElementById("dropdownNavbar")
+      const dropdownNavbarLink = document.getElementById("dropdownNavbarLink")
+
+      if (
+        dropdownNavbar &&
+        dropdownNavbarLink &&
+        !dropdownNavbar.contains(event.target as Node) &&
+        !dropdownNavbarLink.contains(event.target as Node)
+      ) {
+        dropdownNavbar.classList.add("hidden")
+        dropdownNavbar.classList.remove("block")
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside)
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside)
+    }
+  }, [])
+
   return (
-    <nav className="border-gray-200 bg-white dark:bg-gray-900">
+    <nav className="sticky top-0 border-gray-200 bg-white drop-shadow-xl dark:bg-gray-900">
       <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4">
-        <a
-          href="https://flowbite.com/"
-          className="flex items-center space-x-3 rtl:space-x-reverse"
-        >
-          {/* <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" /> */}
+        <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
           <span className="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">
-            Flowbite
+            MyNotes
           </span>
         </a>
-        <div className="flex md:order-2">
-          <button
-            type="button"
-            data-collapse-toggle="navbar-search"
-            aria-controls="navbar-search"
-            aria-expanded="false"
-            className="me-1 rounded-lg p-2.5 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-700 md:hidden"
-          >
-            <svg
-              className="h-5 w-5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 20 20"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-              />
-            </svg>
-            <span className="sr-only">Search</span>
-          </button>
-          <div className="relative hidden md:block">
+        <div className="flex md:order-1">
+          <div className="relative hidden w-96 md:block">
             <div className="pointer-events-none absolute inset-y-0 start-0 flex items-center ps-3">
-              <svg
-                className="h-4 w-4 text-gray-500 dark:text-gray-400"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 20 20"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                />
-              </svg>
-              <span className="sr-only">Search icon</span>
+              <BiSearch className="h-4 w-4 text-gray-500 dark:text-gray-400" />
             </div>
             <input
               type="text"
@@ -67,6 +75,7 @@ const Header = () => {
             />
           </div>
           <button
+            onClick={handleClick}
             data-collapse-toggle="navbar-search"
             type="button"
             className="inline-flex h-10 w-10 items-center justify-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"
@@ -74,21 +83,7 @@ const Header = () => {
             aria-expanded="false"
           >
             <span className="sr-only">Open main menu</span>
-            <svg
-              className="h-5 w-5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 17 14"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M1 1h15M1 7h15M1 13h15"
-              />
-            </svg>
+            <PiListBold className="h-6 w-6" />
           </button>
         </div>
         <div
@@ -97,21 +92,7 @@ const Header = () => {
         >
           <div className="relative mt-3 md:hidden">
             <div className="pointer-events-none absolute inset-y-0 start-0 flex items-center ps-3">
-              <svg
-                className="h-4 w-4 text-gray-500 dark:text-gray-400"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 20 20"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                />
-              </svg>
+              <BiSearch className="h-4 w-4 text-gray-500 dark:text-gray-400" />
             </div>
             <input
               type="text"
@@ -122,29 +103,43 @@ const Header = () => {
           </div>
           <ul className="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium rtl:space-x-reverse dark:border-gray-700 dark:bg-gray-800 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 md:dark:bg-gray-900">
             <li>
-              <a
-                href="#"
-                className="block rounded bg-blue-700 px-3 py-2 text-white md:bg-transparent md:p-0 md:text-blue-700 md:dark:text-blue-500"
-                aria-current="page"
+              <button
+                onClick={dropdown}
+                id="dropdownNavbarLink"
+                data-dropdown-toggle="dropdownNavbar"
+                className="flex w-full items-center justify-between rounded px-3 py-2 text-gray-900 hover:bg-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:focus:text-white md:w-auto md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
               >
-                Home
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
+                {/* <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 me-2" alt="Flowbite Logo" /> */}
+                <HiUserCircle className="me-2 h-8 w-8" />
+                Eren Jaeger
+                <IoIosArrowDown className="ms-2.5" />
+              </button>
+              <div
+                id="dropdownNavbar"
+                className="absolute z-10 mt-1 hidden w-44 divide-y divide-gray-100 rounded-lg bg-white font-normal shadow dark:divide-gray-600 dark:bg-gray-700"
               >
-                About
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
-              >
-                Services
-              </a>
+                <ul
+                  className="py-2 text-sm text-gray-700 dark:text-gray-400"
+                  aria-labelledby="dropdownLargeButton"
+                >
+                  <li>
+                    <a
+                      href="#"
+                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                    >
+                      Settings
+                    </a>
+                  </li>
+                </ul>
+                <div className="py-1">
+                  <a
+                    href="#"
+                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    Sign out
+                  </a>
+                </div>
+              </div>
             </li>
           </ul>
         </div>
